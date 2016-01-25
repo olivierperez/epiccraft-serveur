e55a73052bba9e1e4c1ffc0bea15a16b878c1300
Olivier Perez <olivier.perez@worldline.com>
2016-01-25 11:34:03 +0100

    Save - Don't keep full path to minecraft directory

--- scripts/save.sh
+++ scripts/save.sh
@@ -1,4 +1,8 @@
 ### Saving party
-sudo tar -cf /var/www/html/save.minecraft.`date +"%y-%m-%d.%H-%M"`.tar /home/admin/minecraft_ram/ ;
-sudo tar -cf /var/www/html/save.minecraft.world.`date +"%y-%m-%d.%H-%M"`.tar /home/admin/minecraft_ram/world/ ;
+cd /home/admin/minecraft_ram/
+sudo tar -cf /var/www/html/save.minecraft.`date +"%y-%m-%d.%H-%M"`.tar ./
+
+cd /home/admin/minecraft_ram/world/
+sudo tar -cf /var/www/html/save.minecraft.world.`date +"%y-%m-%d.%H-%M"`.tar ./
+
 echo "Download the save at http://`dig +short myip.opendns.com @resolver1.opendns.com`/"
